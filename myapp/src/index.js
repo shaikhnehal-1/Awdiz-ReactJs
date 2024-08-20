b520acb6601ae2b068dc24a026364d30a2c5fb91
shaikhnehal-1 <shaikhnehal.digital@gmail.com>
2024-08-20 22:55:16 +0530

    ok

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 // import { MyApp, MyApp2 } from './App';
+
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
